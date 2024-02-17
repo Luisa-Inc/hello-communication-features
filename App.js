1e36b976af988f31b61bd79eb654095f8cf04125
Luisa Ihle <luisa.ihle@gmail.com>
2024-02-17 11:32:37 +0100

    Update app.js
    initialize image-picker

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, View, StyleSheet, Image } from "react-native";
 
-export default function App() {
+import * as ImagePicker from "expo-image-picker";
+
+const App = () => {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) setImage(result.assets[0]);
+      else setImage(null);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Pick an image from the library" onPress={pickImage} />
+      <Button title="Take a Photo" onPress={() => {}} />
+
+      {image && (
+        <Image
+          source={{ uri: image.uri }}
+          style={{ width: 200, height: 200 }}
+        />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
   },
 });
+
+export default App;
